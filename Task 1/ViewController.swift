094574eb68e4e57b2e8a0b5996bcc8014f1b847e
OgabekDev <ogabekdev@gmail.com>
2023-01-05 11:41:34 +0500

    Initial Commit

---
+++ Task 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 1
+//
+//  Created by Ogabek Matyakubov on 05/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
